b511bfec4f6c86aed027903ee146d5ce4eb80e7c
WhitetailAni <lesbians@cerulean.fm>
2024-12-24 23:36:17 -0600

    fix pace overlays finally

--- Carmine/Subclasses/Pace/PTMapView.swift
+++ Carmine/Subclasses/Pace/PTMapView.swift
@@ -65,16 +65,21 @@ class PTMapView: MKMapView {
         }
         
         DispatchQueue.global().async {
-            let coordinateArray = PaceAPI().getPolyLineForRouteID(routeID: self.mark.route?.id ?? 0)
-            let overlay = PTPolyline(coordinates: coordinateArray, count: coordinateArray.count)
-            overlay.isPulse = false
+            let coordinateArrays = PaceAPI().getPolyLineForRouteID(routeID: self.mark.route?.id ?? 0)
+            var overlayArray: [PTPolyline] = []
             
-            if self.mark.route?.number ?? 404 < 150 {
-                overlay.isPulse = true
+            for coordinateArray in coordinateArrays {
+                let overlay = PTPolyline(coordinates: coordinateArray, count: coordinateArray.count)
+                overlay.isPulse = false
+                
+                if self.mark.route?.number ?? 404 < 150 {
+                    overlay.isPulse = true
+                }
+                
+                overlayArray.append(overlay)
             }
-            
             DispatchQueue.main.sync {
-                self.addOverlay(overlay)
+                self.addOverlays(overlayArray)
             }
         }
         
